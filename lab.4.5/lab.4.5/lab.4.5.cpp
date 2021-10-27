d7a2b198045edc8e59afa70e5afbe606c611d4b1
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2021-10-27 15:09:00 +0300

    Add files via upload

---
+++ lab.4.5/lab.4.5/lab.4.5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		if ((y <= sqrt(R * R - (x - R) * (x - R)) && (y >= 0)) ||
+			((y >= sqrt(R * R - (x + R) * (x + R)) - R) && (y <= 0) && (x <= 0)))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+
+		for (int i = 0; i < 10; i++)
+		{
+			x = 6. * rand() / RAND_MAX - 3;
+			y = 6. * rand() / RAND_MAX - 3;
+			if ((y <= sqrt(R * R - (x - R) * (x - R)) && (y >= 0)) ||
+				((y >= sqrt(R * R - (x + R) * (x + R)) - R) && (y <= 0) && (x <= 0)))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
